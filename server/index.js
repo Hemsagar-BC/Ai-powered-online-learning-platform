2e73e6b5d22e6af51c3f19ebd5299f6a5100aef0
Hemsagar-BC <hemsagarbc1@gmail.com>
2025-11-24 21:51:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors({
     'http://localhost:5175', 
     'http://localhost:5176', 
     'http://localhost:4173',
-    'https://ai-powered-online-learning-platform-p5izl0o82.vercel.app'
+    'https://ai-powered-online-learning-platform-coral.vercel.app/'
   ],
   credentials: true
 }));
